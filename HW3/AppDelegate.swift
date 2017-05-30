4acdf79e92b5e3116875d23bdfae9f7f56cb42d8
cye2277 <cye2277@gmail.com>
2017-05-30 19:12:26 +0800

    no message

---
+++ HW3/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  HW3
+//
+//  Created by MAC on 2017/5/29.
+//  Copyright © 2017年 MAC. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
